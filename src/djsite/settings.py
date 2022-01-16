bd4e4a6655f7cceea232334bef8df0d7baca4f27
Egor Ivanov <thunderquack@mail.ru>
2022-01-16 19:07:58 +0100

    works

--- src/djsite/settings.py
+++ src/djsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'djsite',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -66,6 +68,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'djsite.wsgi.application'
 
---
+++ src/djsite/src/views.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import os
+from django.http import HttpRequest
+from django.shortcuts import render
+
+TEMPLATES_DIR = '../templates'
+
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    s = os.path.isfile('djsite/templates/index.html')
+    return render(
+        request,
+        os.path.join(TEMPLATES_DIR, 'index.html'),
+        {
+            'title': 'Home Page',
+            'year': datetime.now().year,
+        }
+    )
