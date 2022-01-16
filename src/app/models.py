04765b8ab3b577a0ed239c9cf9e60d89a8bd6fff
Egor Ivanov <thunderquack@mail.ru>
2022-01-16 22:32:32 +0100

    tags

--- src/app/models.py
+++ src/app/models.py
@@ -10,3 +10,8 @@ class Post(models.Model):
     header = models.CharField(max_length=256)
     post = models.TextField()
     date = models.DateTimeField()
+    tags = models.ManyToManyField('Tag', blank=True)
+
+class Tag(models.Model):
+    text = models.CharField(max_length=128)
+    posts = models.ManyToManyField('Post', blank=True, through=Post.tags.through)
