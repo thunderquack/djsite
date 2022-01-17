ab9822da5fdb96fe33d852838df70bb07b191b35
Egor Ivanov <thunderquack@mail.ru>
2022-01-17 11:01:42 +0100

    static image

--- src/app/views.py
+++ src/app/views.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 import os
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+
+from app.models import Post
 
 TEMPLATES_DIR = '../templates'
 
@@ -9,11 +11,19 @@ TEMPLATES_DIR = '../templates'
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
+
     return render(
         request,
         os.path.join(TEMPLATES_DIR, 'index.html'),
         {
             'title': 'Home Page',
             'year': datetime.now().year,
+            'blogposts': Post.objects.all
         }
     )
+
+
+def create_post(request):
+    """Creates post"""
+    assert isinstance(request, HttpRequest)
+    return HttpResponse('')
